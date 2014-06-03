300881ef9c01b775cf9743ed372c7121816ea2d4
FengZongqiang <zongqiangf@gmail.com>
2014-06-03 09:22:32 +0800

    some files which are not used are deleted like exti.c pwm_output.c etc.

--- USER/pwm_output.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PWM_OUTPUT_H
-#define	__PWM_OUTPUT_H
-
-#include "stm32f10x.h"
-
-void TIM3_PWM_Config(void);
-
-#endif /* __PWM_OUTPUT_H */
-
--- USER/timer.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TIMER_H
-#define __TIMER_H
-#include "stm32f10x.h"
-
-void TIM5_Cap_Init(u16 arr,u16 psc);
-
-#endif
